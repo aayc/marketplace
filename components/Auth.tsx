d1b48e88816d66c792a0ce21afcc62eb3491ae84
Aaron Chan <aaron.y.chan64@gmail.com>
2022-05-30 22:51:06 -0600

    Fix sign in

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -4,13 +4,27 @@ import { supabase } from "../utils/supabaseClient";
 export default function Auth() {
   const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleLogin = async (email: string) => {
+  const handleLogin = async (email: string, password: string) => {
     try {
       setLoading(true);
-      const { error } = await supabase.auth.signIn({ email });
+      const result = await supabase.auth.signIn({ email, password });
+      console.log(result);
+      const { error } = result;
       if (error) throw error;
-      alert("Check your email for the login link!");
+    } catch (error: any) {
+      alert(error.error_description || error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleRegister = async (email: string, password: string) => {
+    try {
+      setLoading(true);
+      const { error } = await supabase.auth.signUp({ email, password });
+      if (error) throw error;
     } catch (error: any) {
       alert(error.error_description || error.message);
     } finally {
@@ -19,32 +33,50 @@ export default function Auth() {
   };
 
   return (
-    <div className="row flex flex-center">
-      <div className="col-6 form-widget">
-        <h1 className="header">Supabase + Next.js</h1>
-        <p className="description">
-          Sign in via magic link with your email below
-        </p>
-        <div>
-          <input
-            className="inputField"
-            type="email"
-            placeholder="Your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+    <div className="flex justify-center">
+      <div className="">
+        <div className="w-full text-center">
+          <h1>Marketplace</h1>
         </div>
-        <div>
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              handleLogin(email);
-            }}
-            className="button block"
-            disabled={loading}
-          >
-            <span>{loading ? "Loading" : "Send magic link"}</span>
-          </button>
+        <div className="shadow-lg p-4 mt-4">
+          <div>
+            <input
+              className="block bg-gray-100 p-2"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              className="block bg-gray-100 p-2 mt-2"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="mt-2 flex">
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                handleLogin(email, password);
+              }}
+              className="py-2 px-4 bg-blue-500 block text-white rounded-lg hover:bg-blue-600"
+              disabled={loading}
+            >
+              <span>{loading ? "Loading" : "Log in"}</span>
+            </button>
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                handleRegister(email, password);
+              }}
+              className="py-2 px-4 bg-blue-500 block ml-2 hover:bg-blue-600 text-white rounded-lg"
+              disabled={loading}
+            >
+              <span>{loading ? "Loading" : "Register"}</span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
